5eb32bcdf20cd3166c0ff147642fdc7dceee0e61
Samizing <100716283+samizing@users.noreply.github.com>
2022-05-31 10:28:02 +0000

    new

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,8 @@
-# Conditional Execution
+hrs = float(input("Enter Hours: "))
+rate = float(input("enter rate: "))
 
-hrs = input("Enter hours? ")
+if (hrs>40):
+    h = hrs-40
+    print(40*rate + h*rate*1.5)
+else :
+    print(hrs*rate)
